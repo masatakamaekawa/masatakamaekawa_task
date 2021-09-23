bc359148338722c21dba384f94cee89e4e967bce
masatakamaekawa <mstkmekw@hito-koto.com>
2021-09-23 09:21:22 +0900

    タスク管理アプリ完了

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// Itemクラスを読み込む
+use App\Models\Task;
+use App\Http\Requests\TaskRequest;
+
+class TaskController extends Controller
+{
+    // indexページへ移動
+    public function index()
+    {
+        // モデル名::テーブル全件取得
+        $tasks = Task::all();
+        // Itemsティレクトリーの中のindexページを指定し、itemsの連想配列を代入
+        return view('tasks.index', ['tasks' => $tasks]);
+    }
+
+    public function store(TaskRequest $request)
+    {
+        // インスタンスの作成
+        $task = new Task;
+        // 値の用意
+        $task->title = $request->title;
+        $task->write = $request->write;
+
+        // インスタンスに値を設定して保存
+        $task->save();
+        // 登録したらindexに戻る
+        return redirect('/tasks');
+    }
+
+    public function show($id)
+    {
+        $task = Task::find($id);
+        return view('tasks.show', ['task' => $task]);
+    }
+
+    public function edit($id) 
+    {
+        $task = Task::find($id);
+        return view('tasks.edit', ['task' => $task]);
+    }
+
+    public function update(TaskRequest $request, $id)
+    {
+        // ここはidで探して持ってくる以外はstoreと同じ
+        $task = Task::find($id);
+        // 値の用意
+        $task->title = $request->title;
+        $task->write = $request->write;
+        // 保存
+        $task->save();
+        // 登録したらindexに戻る
+        return redirect('/tasks');
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+        return redirect('/taaks');
+    }
+}
